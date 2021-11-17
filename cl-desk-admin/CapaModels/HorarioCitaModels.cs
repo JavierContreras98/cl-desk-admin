bdee90a6b4de25af6346e3c0752afb1d7dfaa150
Mauricio Arevalo <mauricioarevalo453@gmail.com>
2021-11-16 22:09:50 -0600

    Crear horarioMoldels

--- cl-desk-admin/CapaModels/HorarioCitaModels.cs
+++ cl-desk-admin/CapaModels/HorarioCitaModels.cs
@@ -8,5 +8,10 @@ namespace cl_desk_admin.CapaModels
 {
     class HorarioCitaModels
     {
+        int id;
+        TimeSpan hora_cita;
+
+        public int Id { get => id; set => id = value; }
+        public TimeSpan Hora_cita { get => hora_cita; set => hora_cita = value; }
     }
 }
