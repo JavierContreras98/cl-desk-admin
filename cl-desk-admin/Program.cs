147400b3f5ac9f7c662dc8f9d4e2d7716b7dc300
Javier Contreras <jecs4711@gmail.com>
2021-11-23 13:36:37 -0600

    CRU de Usuario funcionado 100%. D(Eliminar) no posee porque se elimina desde el empleado

--- cl-desk-admin/Program.cs
+++ cl-desk-admin/Program.cs
@@ -16,7 +16,7 @@ namespace cl_desk_admin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CapaVista.ViewCompartidas.AdminInforMedicos.frmAdminInforMedicos());
+            Application.Run(new CapaVista.ViewAdministradorGeneral.AdminUsuario.frmAdministrarUsuario());
         }
     }
 }
