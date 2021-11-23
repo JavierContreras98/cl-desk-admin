981a3f45697873f096bb533d0f1bda8513534b0b
joseabel__Jr <57048755+JoseAbelCF@users.noreply.github.com>
2021-11-22 20:21:51 -0600

    Quitando RUN

--- cl-desk-admin/Program.cs
+++ cl-desk-admin/Program.cs
@@ -16,7 +16,7 @@ namespace cl_desk_admin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CapaVista.ViewCompartidas.AdminDepartamentos.frmAdministrarDepartamentos());
+            Application.Run();
         }
     }
 }
