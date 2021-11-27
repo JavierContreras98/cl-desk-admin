dfaf0c480812c48d4f74e012c771784ee8609ae3
joseabel__Jr <57048755+JoseAbelCF@users.noreply.github.com>
2021-11-26 22:07:38 -0600

    Reporte empleado funcional

---
+++ cl-desk-admin/CapaVista/ViewReportes/ReporteEmpleados.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace cl_desk_admin.CapaVista.ViewReportes
+{
+    partial class ReporteEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dataSetPrincipal = new cl_desk_admin.DataSetPrincipal();
+            this.sPCREATEREPORT4BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sP_CREATE_REPORT4TableAdapter = new cl_desk_admin.DataSetPrincipalTableAdapters.SP_CREATE_REPORT4TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetPrincipal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sPCREATEREPORT4BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.sPCREATEREPORT4BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "cl_desk_admin.ViewInforme.ReporteEmpleados.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1025, 612);
+            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // dataSetPrincipal
+            // 
+            this.dataSetPrincipal.DataSetName = "DataSetPrincipal";
+            this.dataSetPrincipal.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sPCREATEREPORT4BindingSource
+            // 
+            this.sPCREATEREPORT4BindingSource.DataMember = "SP_CREATE_REPORT4";
+            this.sPCREATEREPORT4BindingSource.DataSource = this.dataSetPrincipal;
+            // 
+            // sP_CREATE_REPORT4TableAdapter
+            // 
+            this.sP_CREATE_REPORT4TableAdapter.ClearBeforeFill = true;
+            // 
+            // ReporteEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1025, 612);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "ReporteEmpleados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReporteEmpleados";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ReporteEmpleados_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetPrincipal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sPCREATEREPORT4BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private DataSetPrincipal dataSetPrincipal;
+        private System.Windows.Forms.BindingSource sPCREATEREPORT4BindingSource;
+        private DataSetPrincipalTableAdapters.SP_CREATE_REPORT4TableAdapter sP_CREATE_REPORT4TableAdapter;
+    }
+}
---
+++ cl-desk-admin/CapaVista/ViewReportes/ReporteEmpleados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cl_desk_admin.CapaVista.ViewReportes
+{
+    public partial class ReporteEmpleados : Form
+    {
+        public ReporteEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteEmpleados_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dataSetPrincipal.SP_CREATE_REPORT4' Puede moverla o quitarla según sea necesario.
+            this.sP_CREATE_REPORT4TableAdapter.Fill(this.dataSetPrincipal.SP_CREATE_REPORT4);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
